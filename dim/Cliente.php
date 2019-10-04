d1de751668d9b53c65008a1020efe2af440f5fda
LuiZeredo <luizeredo@gmail.com>
2019-10-04 20:26:00 -0300

    Implementado o Model para Cliente e Inciada a classe da carga da dimensão Cliente

---
+++ dim/Cliente.php
@@ -0,0 +1,80 @@
+<?php
+    namespace DIM;
+
+    /**
+        *Model da entidade Cliente;
+        *@author Luiz André Zeredo    
+    */
+
+    class Cliente {
+        /**
+            *CPF do Cliente
+            *@var string    
+        */
+        public $cpf;
+        /**
+            *Nome do Cliente
+            *@var string    
+        */
+        public $nome;
+        /**
+            *Sexo do Cliente
+            *@var string    
+        */
+        public $sexo;
+        /**
+            *Idade do Cliente
+            *@var string    
+        */
+        public $idade;
+        /**
+            *Email do Cliente
+            *@var string    
+        */
+        public $email;
+        /**
+            *Rua do Cliente
+            *@var string    
+        */
+        public $rua;
+        /**
+            *Bairro do Cliente
+            *@var string    
+        */
+        public $bairro;
+        /**
+            *Cidade do Cliente
+            *@var string    
+        */
+        public $cidade;
+        /**
+            *UF do Cliente
+            *@var string    
+        */
+        public $uf;
+
+        /**
+            *Carrega os atributos da Classe Prospect
+            *@param $cpf CPF do Cliente 
+            *@param $nome Nome do Cliente
+            *@param $sexo Sexo do Cliente
+            *@param $idade Idade do Cliente
+            *@param $rua Rua do Cliente
+            *@param $bairro Bairro do Cliente
+            *@param $cidade Cidade do Cliente
+            *@param $uf UF do Cliente
+            *@return Void
+        */
+        public function setProspect($cpf, $nome, $sexo, $idade, $rua, $bairro, $cidade, $uf){
+            $this->$cpf= $cpf;
+            $this->$nome = $nome ;
+            $this->$sexo = $sexo;
+            $this->$idade = $idade;
+            $this->$rua = $rua;
+            $this->$bairro = $bairro;
+            $this->$cidade = $cidade;
+            $this->$uf = $uf;
+        }
+        
+    }
+?>
--- instrucoes/index.php
+++ instrucoes/index.php
@@ -1,9 +1,10 @@
 <?php
-$numero = 5; //Número para calcular o fatorial
-$fatorial = 1;
-while ($numero > 1){
-    $fatorial *= $numero;
-    $numero--;
-}
 
-echo $fatorial;
+
+
+
+
+
+
+
+?>
