1c2e10e09227e3d1b08cfd73c22e40363f6ef1bd
jessica <jessicaduarte681@gmail.com>
2019-11-14 22:22:35 -0200

    Código

--- dim/Sumario.php
+++ dim/Sumario.php
@@ -1,18 +1,16 @@
 <?php
-
 namespace dimensoes;
-
 class Sumario{
-    public $quantidadeInclusoes;
-    public $quantidadeAlteracoes;
-
-    public function setQuantidadeInclusoes(){
-        $this->quantidadeInclusoes ++;
-    }
-    public function setQuantidadeAlteracoes(){
-        $this->quantidadeAlteracoes++;
-    }
+   public $quantidadeInclusoes;
+   public $quantidadeAlteracoes;
+   function __construct(){
+      $this->quantidadeInclusoes = 0;
+      $this->quantidadeAlteracoes = 0;
+   }
+   public function setQuantidadeInclusoes(){
+      $this->quantidadeInclusoes ++;
+   }
+   public function setQuantidadeAlteracoes(){
+      $this->quantidadeAlteracoes ++;
+   }
 }
-
-
-?>
--- dim/executar.php
+++ dim/executar.php
@@ -1,13 +1,24 @@
 <?php
 require_once('DimCliente.php');
+require_once('DimData.php');
+require_once('Sumario.php');
 
+use dimensoes\Sumario;
+use dimensoes\DimData;
 use dimensoes\DimCliente;
 
 $dimCliente = new DimCliente();
 $sumCliente = $dimCliente->carregarDimCliente();
-echo $sumCliente->quantidadeInclusoes;
+echo "Clientes: <br>";
+echo "Inclusões: ".$sumCliente->quantidadeInclusoes."<br>";
+echo "Alterações: ".$sumCliente->quantidadeAlteracoes;
+echo "<br>============================================<br>";
 
-
-
+$dimData = new DimData();
+$sumData = $dimData->extrairETransformarDatas();
+echo "Data: <br>";
+echo "Inclusões: ".$sumData->quantidadeInclusoes."<br>";
+echo "Alterações: ".$sumData->quantidadeAlteracoes;
+echo "<br>============================================<br>";
 
 ?>
